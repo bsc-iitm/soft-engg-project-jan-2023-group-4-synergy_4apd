5cbc7450c2f0297406b44f4f0495b16ab737afba
papa-delta <125729402+papa-delta@users.noreply.github.com>
2023-04-09 14:18:24 +0530

    Corrected tag controller and imported api into main.py

--- src/backend/api/tag.py
+++ src/backend/api/tag.py
@@ -1,24 +1,14 @@
-# from flask_restful import reqparse,Resource
-# from backend.models.tag import *
-# from flask_cors import CORS,cross_origin
-
-from flask import *
-from flask_sqlalchemy import *
-from flask_cors import *
-from flask_restful import *
-from models import *
-
+from flask_restful import reqparse,Resource
+from backend.models import *
 
 create_tag_parser=reqparse.RequestParser()
 create_tag_parser.add_argument('name')
 create_tag_parser.add_argument('description')
 
-
 modify_tag_parser=reqparse.RequestParser()
 modify_tag_parser.add_argument('tagID')
 modify_tag_parser.add_argument('name')
 modify_tag_parser.add_argument('description')
-
 
 get_tag_parser=reqparse.RequestParser()
 get_tag_parser.add_argument('tagIDList')
@@ -39,7 +29,6 @@ class TagAPI(Resource):
     
     # def post(self,)
     
-
 
 
     
--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 from backend.config import Config
 from backend.database import *
 from backend.models import *
+from backend.api import *
+
 
 
 app = Flask(__name__, template_folder='templates')
@@ -17,7 +19,10 @@ db.init_app(app)
 api = Api(app)
 cors = CORS(app)
 
+api.add_resource(TagAPI,"/api/v1/tag")
+
 
 if __name__ == '__main__':
     db.create_all()
     app.run(host='0.0.0.0', port=8000)
+
