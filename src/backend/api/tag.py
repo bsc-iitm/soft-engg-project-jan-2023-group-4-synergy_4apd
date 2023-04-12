6680a2cb406a314b3dda01d8acec709ea973817c
papa-delta <125729402+papa-delta@users.noreply.github.com>
2023-04-12 10:39:09 +0530

    Added POST request handler for tags controller

--- src/backend/api/tag.py
+++ src/backend/api/tag.py
@@ -22,7 +22,7 @@ class TagAPI(Resource):
     def get(self):
         
         d=get_tag_parser.parse_args()
-        ret = d.get('tagIDList')
+        ret = d.get('tagIDList',None)
         
         if ret=='' or ret is None:
             return {"status":400,"message":"Malformed request!"},400
@@ -57,8 +57,25 @@ class TagAPI(Resource):
         
         return {"status":200,"message":"Request successful","tags":retdict},200
 
-    # def post(self,)
-    
+    def post(self):
+        d=create_tag_parser.parse_args()
+        tagName,tagDescription=d.get('name',None),d.get('description',None)
+
+        if tagName=='' or tagName is None:
+            return {"status":400,"message":"Malformed request!"},400
+
+        x=Tag.query.filter_by(name=tagName).first()
+
+        if tagName != x.name:
+        
+            dbObj=Tag(name=tagName,description=tagDescription)
+            db.session.add(dbObj)
+            db.session.commit()
+
+            return {"status":201,"message":"Request successful"},201
+        else:
+            return {"status":200,"message":"Tag already exists!"},200
+
 
 
     
