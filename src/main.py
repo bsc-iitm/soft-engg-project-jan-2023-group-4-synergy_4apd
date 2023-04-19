b8cea34efb7e8b484caa8506df61a723384e79f4
Afnan Ahmad <34241491+afnan-ahmad@users.noreply.github.com>
2023-04-19 13:52:49 +0530

    Added trigger to add default role

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from flask_restful import Api
 from flask_cors import CORS
 
-from backend import app
+from backend import app, user_datastore
 from backend.database import db
 #from backend.controllers import *
 from backend.api import *
 
 from humanize import naturaltime
+
+import flask_login
 
 
 api = Api(app)
@@ -27,17 +29,23 @@ api.add_resource(RoleAPI, '/api/v1/admin/users/<string:user_id>/')
 def utility_functions():
     return dict(naturaltime=naturaltime)
 
+def assign_default_role(app, user):
+    if len(user.roles) == 0:
+        default_role = user_datastore.find_role('user')
+        user_datastore.add_role_to_user(user, default_role)
+
+flask_login.user_logged_in.connect(assign_default_role)
+
 if __name__ == '__main__':
 
     db.create_all()
 
     roles = Role.query.all()
     if len(roles) == 0:
-
-        user_student=Role(name="user",description="Default users, mainly students")
-        support_staff=Role(name="support_staff",description="Support Staff")
-        admin=Role(name="admin",description="Administrator")
-        superadmin=Role(name="superadmin",description="Super Administrator")
+        user_student=Role(name='user', description='Default users, mainly students')
+        support_staff=Role(name='support_staff', description='Support Staff')
+        admin=Role(name='admin', description='Administrator')
+        superadmin=Role(name='superadmin', description='Super Administrator')
 
         db.session.add(user_student)
         db.session.add(support_staff)
